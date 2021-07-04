20d724dcd2d4968d48664e3d9dff51ab6b424e8c
dragonmc77 <dragonmc77@gmail.com>
2021-07-04 00:18:26 -0400

    Initial calibre plugin files

---
+++ config.py
@@ -0,0 +1,30 @@
+from calibre.utils.config import JSONConfig
+
+# This is where all preferences for this plugin will be stored
+# Remember that this name (i.e. plugins/linkcomics) is also
+# in a global namespace, so make it as unique as possible.
+# You should always prefix your config file name with plugins/,
+# so as to ensure you dont accidentally clobber a calibre config file
+prefs = JSONConfig('plugins/linkcomics')
+
+# Set defaults
+prefs.defaults['hello_world_msg'] = 'Hello, World!'
+
+
+class ConfigWidget(QWidget):
+
+    def __init__(self):
+        QWidget.__init__(self)
+        self.l = QHBoxLayout()
+        self.setLayout(self.l)
+
+        self.label = QLabel('Hello world &message:')
+        self.l.addWidget(self.label)
+
+        self.msg = QLineEdit(self)
+        self.msg.setText(prefs['hello_world_msg'])
+        self.l.addWidget(self.msg)
+        self.label.setBuddy(self.msg)
+
+    def save_settings(self):
+        prefs['hello_world_msg'] = self.msg.text()
